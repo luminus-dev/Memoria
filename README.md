d85a67a49cde0e5381402bbb91c9113ecc7dc796
eleline <ggggepicstar@gmail.com>
2020-03-19 23:37:06 +0900

    first commit 😍

---
+++ README.md
@@ -0,0 +1,3 @@
+# Memoria
+
+[WIP]
---
+++ eslintrc.js
@@ -0,0 +1,40 @@
+module.exports = {
+  root: true,
+  parser: "vue-eslint-parser",
+  env: {
+    browser: true,
+    node: true
+  },
+  parserOptions: {
+    // parser: '@typescript-eslint/parser'
+  },
+  extends: [
+    "@nuxtjs",
+    "@nuxtjs/eslint-config-typescript", // <= これを追加
+    "plugin:nuxt/recommended",
+    "plugin:prettier/recommended",
+    "prettier",
+    "prettier/vue"
+  ],
+  plugins: ["prettier"],
+  // add your custom rules here
+  rules: {
+    "no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars": "error",
+    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+    "vue/singleline-html-element-content-newline": "off",
+    "vue/no-v-html": "off",
+    "vue/html-closing-bracket-spacing": "off",
+    "vue/max-attributes-per-line": [
+      2,
+      {
+        singleline: 5,
+        multiline: {
+          max: 1,
+          allowFirstLine: false
+        }
+      }
+    ]
+  }
+};
